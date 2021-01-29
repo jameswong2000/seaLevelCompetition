a944e37f004bcb21100cb2ed23a6601ec28b80a6
James Wong <wongwongjames@yahoo.com.hk>
2021-01-30 02:32:22 +0800

    add Bluetooth module

--- main.ino
+++ main.ino
@@ -5,17 +5,22 @@ const int chipSelect = 10;
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <NewPing.h>
+#include <SoftwareSerial.h>
+
 #define TRIGGER_PIN  9  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     8  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 300 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+SoftwareSerial BT(11, 12);  // RX, TX
+
 int datanum = 0;
 int numfile = 0;
 char fname[12];
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
+  BT.begin(9600);
   //syn.Slaveboudset(19200);
   //Serial.begin(19200);
   lcd.init();
@@ -24,13 +29,12 @@ void setup() {
   lcd.print("Welcome!");
   delay(2000);
   lcd.clear();
-  pinMode (2, OUTPUT); //attach pin 2 to vcc
-  pinMode (5, OUTPUT); //attach pin 5 to GND
   // initialize serial communication:
   Serial.begin(9600);
   
 
   Serial.print("Initializing SD card...");
+  Serial.println("");
 
   // make sure that the default chip select pin is set to
   // output, even if you don't use it:
@@ -38,11 +42,11 @@ void setup() {
 
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
+    //Serial.println("Card failed, or not present");
     // don't do anything more:
     return;
   }
-  Serial.println("Card initialized.");
+  //Serial.println("Card initialized.");
 }
 
 void loop()
@@ -61,11 +65,12 @@ void loop()
   if((d) <= 300) {
     if (28 - (d) >0) {
       Serial.print(28 - (d),0); // Convert ping time to distance in cm and print result
-      Serial.println("cm");
+      //Serial.println("cm");
       lcd.clear();
       lcd.print("Sea Level: ");
       lcd.print(28 - (d),0);
       lcd.print("cm");
+      BT.print(28 - (d),0);
     }  
     else {
       Serial.println("0cm");
@@ -73,6 +78,7 @@ void loop()
       lcd.print("Sea Level: ");
       lcd.print("0");
       lcd.print("cm");
+      BT.print(0);
     }
   }
   if (d > 300) {
@@ -80,8 +86,10 @@ void loop()
     lcd.clear();
     lcd.print("Sea Level: ");
     lcd.print("OUT");
+    BT.print("OUT");
   }
   
+    
   Serial.println("");
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
@@ -89,15 +97,15 @@ void loop()
 
   // if the file is available, write to it:
   if (dataFile) {
-      dataFile.print(28 - d,0);
+    dataFile.print(28 - d,0);
     //dataFile.print("cm");
     dataFile.println("");
-    Serial.println("One data was written.");
-    Serial.print("Number of data: ");
-    Serial.println(datanum++);
+    //Serial.println("One data was written.");
+    //Serial.print("Number of data: ");
+    //Serial.println(datanum++);
     sprintf(fname, "data%d.csv",numfile);
-    Serial.println(fname);
-    Serial.println("");
+    //Serial.println(fname);
+    //Serial.println("");
     SD.open(fname, FILE_WRITE);
     if (datanum == 10) {numfile++; datanum = 0;}
     dataFile.flush();
@@ -106,8 +114,8 @@ void loop()
   // if the file isn't open, pop up an error:
   else {
    Serial.println();
-    Serial.println("error opening datalog.csv");
-    Serial.println("");
+    //Serial.println("error opening datalog.csv");
+    //Serial.println("");
   }
 
   /* ******************** End of Printing Result (both LCD and SD File) ********************* */
